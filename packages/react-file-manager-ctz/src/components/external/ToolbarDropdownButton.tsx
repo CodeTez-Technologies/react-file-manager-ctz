d54553757c08cf1fd3e5b2ad2e51b28badd06eac
pradeepanCodetez <53948047+pradeepanCodetez@users.noreply.github.com>
2025-02-21 17:38:42 +0530

    updated

--- packages/react-file-manager-ctz/src/components/external/ToolbarDropdownButton.tsx
+++ packages/react-file-manager-ctz/src/components/external/ToolbarDropdownButton.tsx
@@ -20,98 +20,98 @@ import { ExplorerIconContext } from '../../util/icon-helper';
 import { c, important, makeGlobalExplorerStyles } from '../../util/styles';
 
 export interface ToolbarDropdownButtonProps {
-  text: string;
-  active?: boolean;
-  icon?: Nullable<IconName | string>;
-  onClick?: () => void;
-  disabled?: boolean;
+    text: string;
+    active?: boolean;
+    icon?: Nullable<IconName | string>;
+    onClick?: () => void;
+    disabled?: boolean;
 }
 
 export const ToolbarDropdownButton = React.forwardRef(
-  (props: ToolbarDropdownButtonProps, ref: React.Ref<HTMLLIElement>) => {
-    const { text, active, icon, onClick, disabled } = props;
-    const classes = useStyles();
-    const ExplorerIcon = useContext(ExplorerIconContext);
+    (props: ToolbarDropdownButtonProps, ref: React.Ref<HTMLLIElement>) => {
+        const { text, active, icon, onClick, disabled } = props;
+        const classes = useStyles();
+        const ExplorerIcon = useContext(ExplorerIconContext);
 
-    const className = c({
-      [classes.contextMenuItem]: true,
-      [classes.baseButton]: true,
-      [classes.activeButton]: active,
-    });
-    return (
-      <MenuItem ref={ref} className={className} onClick={onClick} disabled={disabled}>
-        {icon && (
-          <ListItemIcon className={classes.icon}>
-            <ExplorerIcon icon={icon} fixedWidth={true} />
-          </ListItemIcon>
-        )}
-        <ListItemText primaryTypographyProps={{ className: classes.text }}>{text}</ListItemText>
-      </MenuItem>
-    );
-  },
+        const className = c({
+            [classes.contextMenuItem]: true,
+            [classes.baseButton]: true,
+            [classes.activeButton]: active,
+        });
+        return (
+            <MenuItem ref={ref} className={className} onClick={onClick} disabled={disabled}>
+                {icon && (
+                    <ListItemIcon className={classes.icon}>
+                        <ExplorerIcon icon={icon} fixedWidth={true} />
+                    </ListItemIcon>
+                )}
+                <ListItemText primaryTypographyProps={{ className: classes.text }}>{text}</ListItemText>
+            </MenuItem>
+        );
+    },
 );
 
 const useStyles = makeGlobalExplorerStyles((theme) => ({
-  baseButton: {
-    lineHeight: important(theme.toolbar.lineHeight),
-    height: important(theme.toolbar.size),
-    minHeight: important('auto'),
-    minWidth: important('auto'),
-    padding: important(theme.toolbar.buttonPadding),
-  },
-  icon: {
-    fontSize: important(theme.toolbar.fontSize),
-    minWidth: important('auto'),
-    color: important('inherit'),
-    marginRight: 8,
-  },
-  text: {
-    fontSize: important(theme.toolbar.fontSize),
-  },
-  activeButton: {
-    color: important(theme.colors.textActive),
-  },
-  contextMenuItem: {
-    lineHeight: '18px !important',
-    padding: '18px 10px !important'
-  }
+    baseButton: {
+        lineHeight: important(theme.toolbar.lineHeight),
+        height: important(theme.toolbar.size),
+        minHeight: important('auto'),
+        minWidth: important('auto'),
+        padding: important(theme.toolbar.buttonPadding),
+    },
+    icon: {
+        fontSize: important(theme.toolbar.fontSize),
+        minWidth: important('auto'),
+        color: important('inherit'),
+        marginRight: 8,
+    },
+    text: {
+        fontSize: important(theme.toolbar.fontSize),
+    },
+    activeButton: {
+        color: important(theme.colors.textActive),
+    },
+    contextMenuItem: {
+        lineHeight: '18px !important',
+        padding: '18px 10px !important'
+    }
 }));
 
 export interface SmartToolbarDropdownButtonProps {
-  fileActionId: string;
-  onClickFollowUp?: () => void;
+    fileActionId: string;
+    onClickFollowUp?: () => void;
 }
 
 export const SmartToolbarDropdownButton = React.forwardRef(
-  (props: SmartToolbarDropdownButtonProps, ref: React.Ref<HTMLLIElement>) => {
-    const { fileActionId, onClickFollowUp } = props;
+    (props: SmartToolbarDropdownButtonProps, ref: React.Ref<HTMLLIElement>) => {
+        const { fileActionId, onClickFollowUp } = props;
 
-    const action = useParamSelector(selectFileActionData, fileActionId);
-    const triggerAction = useFileActionTrigger(fileActionId);
-    const { icon, active, disabled } = useFileActionProps(fileActionId);
-    const { buttonName } = useLocalizedFileActionStrings(action);
+        const action = useParamSelector(selectFileActionData, fileActionId);
+        const triggerAction = useFileActionTrigger(fileActionId);
+        const { icon, active, disabled } = useFileActionProps(fileActionId);
+        const { buttonName } = useLocalizedFileActionStrings(action);
 
-    // Combine external click handler with internal one
-    const handleClick = useCallback(() => {
-      triggerAction();
-      if (onClickFollowUp) onClickFollowUp();
-    }, [onClickFollowUp, triggerAction]);
+        // Combine external click handler with internal one
+        const handleClick = useCallback(() => {
+            triggerAction();
+            if (onClickFollowUp) onClickFollowUp();
+        }, [onClickFollowUp, triggerAction]);
 
-    if (!action) return null;
-    const { button } = action;
-    if (!button) return null;
-    if (action.customVisibility !== undefined && action.customVisibility() === CustomVisibilityState.Hidden)
-      return null;
+        if (!action) return null;
+        const { button } = action;
+        if (!button) return null;
+        if (action.customVisibility !== undefined && action.customVisibility() === CustomVisibilityState.Hidden)
+            return null;
 
-    return (
-      <ToolbarDropdownButton
-        ref={ref}
-        text={buttonName}
-        icon={icon}
-        onClick={handleClick}
-        active={active}
-        disabled={disabled}
-      />
-    );
-  },
+        return (
+            <ToolbarDropdownButton
+                ref={ref}
+                text={buttonName}
+                icon={icon}
+                onClick={handleClick}
+                active={active}
+                disabled={disabled}
+            />
+        );
+    },
 );
